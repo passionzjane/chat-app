1905fa85472bf328a5d3976e51e8ea1cc3aeca7f
JaneStack <mrumunjanet@gmail.com>
2021-12-13 12:26:34 -0800

    Time stamp integrated in share location

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -6,6 +6,15 @@ const generateMessage = (text) => {
 }
 
 
+const generateLocationMessage = (url) => {
+    return {
+        url,
+        createdAt: new Date().getTime()
+    }
+}
+
+
 module.exports = {
-    generateMessage
+    generateMessage,
+    generateLocationMessage
 }
