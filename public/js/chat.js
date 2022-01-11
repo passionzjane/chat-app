4abb4af86e006cf665522c6ff1f4ddaec1b522bf
JaneStack <mrumunjanet@gmail.com>
2022-01-11 07:08:05 -0800

    user list rendering completed

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,6 +10,7 @@ const $messages = document.querySelector('#messages')
 // Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
 // Options
 const { username, room} = Qs.parse(location.search, { ignoreQueryPrefix: true})
@@ -25,13 +26,21 @@ socket.on('message', (message) => {
 })
 
 socket.on('locationMessage', (message) => {
-    console.log(message)
+    console.log(message) 
     const html = Mustache.render(locationMessageTemplate, {
         username: message.username,
         url: message.url,
         createdAt: moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('roomData', ({ room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
 })
 
 $messageForm.addEventListener('submit', (e) => {
